5297e6ce2d8c94960d064762cbb478cba03f1bd1
Atamert Ölçgen <muhuk@muhuk.com>
2010-12-26 21:17:26 +0800

    Added an example project

---
+++ example/settings.py
@@ -0,0 +1,37 @@
+import os
+
+
+_PATH = os.path.abspath(os.path.dirname(__file__))
+_MODULE = os.path.basename(_PATH)
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'example.db'
+    }
+}
+
+ROOT_URLCONF = _MODULE + '.urls'
+
+MEDIA_ROOT = os.path.join(_PATH, 'media')
+MEDIA_URL = '/media/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+TEMPLATE_DIRS = (
+    os.path.join(_PATH, 'templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'jsonrpc',
+)
+
+SECRET_KEY = 'ye39vq9d@d_tm#!hn^$+%9--8k*+gg12jjqru_pl$x7=_6mq^c'
---
+++ example/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from jsonrpc import jsonrpc_site
+
+
+urlpatterns = patterns('',
+    (r'^json/', jsonrpc_site.dispatch, {}, "jsonrpc_mountpoint"),
+)
+
+
+if settings.DEBUG:
+    from django.views.static import serve
+    _media_url = settings.MEDIA_URL
+    if _media_url.startswith('/'):
+        _media_url = _media_url[1:]
+        urlpatterns += patterns('',
+                                (r'^%s(?P<path>.*)$' % _media_url,
+                                serve,
+                                {'document_root': settings.MEDIA_ROOT}))
+    del(_media_url, serve)
