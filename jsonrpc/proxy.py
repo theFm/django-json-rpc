e41d75fa39cabdcf16e4cbaedaf90875c0cd45af
Atamert Ölçgen <muhuk@muhuk.com>
2010-12-27 22:23:19 +0800

    Made one JSON-RPC test actually work using Django TestCase

--- jsonrpc/proxy.py
+++ jsonrpc/proxy.py
@@ -1,5 +1,6 @@
 import urllib
 import uuid
+from django.test.client import Client
 from jsonrpc._json import loads, dumps
 from jsonrpc.types import Any, Object
 
@@ -36,6 +37,45 @@ class ServiceProxy(object):
                                 'id': str(uuid.uuid1()),
                            })).read()
         y = loads(r)
+        if y.get("error", None):
+            try:
+                from django.conf import settings
+                if settings.DEBUG:
+                    print '%s error %r' % (self.__service_name, y)
+            except:
+                pass
+        return y
+
+
+class TestServiceProxy(object):
+    def __init__(self, service_url, service_name=None, version='1.0'):
+        self.__version = str(version)
+        self.__service_url = service_url
+        self.__service_name = service_name
+        self.client = Client()
+
+    def __getattr__(self, name):
+        if self.__service_name != None:
+            name = "%s.%s" % (self.__service_name, name)
+        return TestServiceProxy(self.__service_url, name, self.__version)
+
+    def __repr__(self):
+        return {"jsonrpc": self.__version,
+                "method": self.__service_name}
+
+    def __call__(self, *args, **kwargs):
+        params = kwargs if len(kwargs) else args
+        if Any.kind(params) == Object and self.__version != '2.0':
+            raise ValueError('Unsupported arg type for JSON-RPC 1.0 ' \
+                             '(the default version for this client, ' \
+                             'pass version="2.0" to use keyword arguments)')
+        response = self.client.post(self.__service_url, dumps({
+                                "jsonrpc": self.__version,
+                                "method": self.__service_name,
+                                'params': params,
+                                'id': str(uuid.uuid1()),
+        }), content_type="application/json")
+        y = loads(response.content)
         if y.get("error", None):
             try:
                 from django.conf import settings
--- jsonrpc/tests/__init__.py
+++ jsonrpc/tests/__init__.py
@@ -1,6 +1,18 @@
-from base import JsonRpcTestCase, TransactionJsonRpcTestCase
+from django.test import TestCase
+from jsonrpc import jsonrpc_method
+from jsonrpc.proxy import TestServiceProxy
 
 
-class SimpleTests(JsonRpcTestCase):
+@jsonrpc_method("echo(message=String) -> String")
+def echo(request, message):
+    return message
+
+
+class SimpleTests(TestCase):
+    def setUp(self):
+        self.proxy = TestServiceProxy("/json/")
+
     def test_simple(self):
-        self.assertEqual(1 + 1, 2)
+        response = self.proxy.echo(u"test")
+        self.assertEqual(response["error"], None)
+        self.assertEqual(response["result"], u"test")
