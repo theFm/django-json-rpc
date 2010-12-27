2e7ffe493da98288eb716fe220cc4d3d9e778fdb
Atamert Ölçgen <muhuk@muhuk.com>
2010-12-27 21:44:10 +0800

    Moved JSON-RPC test classes into a seperate module

--- jsonrpc/tests/__init__.py
+++ jsonrpc/tests/__init__.py
@@ -1,19 +1,4 @@
-from django.test import TestCase, TransactionTestCase
-from django.test.client import Client
-
-
-class JsonRpcTestMixin(object):
-    def _pre_setup(self):
-        self.client = Client()
-        super(JsonRpcTestMixin, self)._pre_setup()
-
-
-class JsonRpcTestCase(TestCase, JsonRpcTestMixin):
-    pass
-
-
-class TransactionJsonRpcTestCase(TransactionTestCase, JsonRpcTestMixin):
-    pass
+from base import JsonRpcTestCase, TransactionJsonRpcTestCase
 
 
 class SimpleTests(JsonRpcTestCase):
