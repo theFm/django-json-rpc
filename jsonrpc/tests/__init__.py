bb011093d5c909484fd797c595d86dd9b2d2952c
Atamert Ölçgen <muhuk@muhuk.com>
2010-12-31 17:20:21 +0800

    Moved functional tests into app

--- jsonrpc/tests/__init__.py
+++ jsonrpc/tests/__init__.py
@@ -1,18 +1,74 @@
+import unittest
 from django.test import TestCase
-from jsonrpc import jsonrpc_method
+from jsonrpc import jsonrpc_method, _parse_sig, Any, SortedDict
+from jsonrpc.exceptions import InvalidParamsError
 from jsonrpc.proxy import TestServiceProxy
+from jsonrpc.site import validate_params
+from jsonrpc.types import String, Object, Array, Nil
 
 
-@jsonrpc_method("echo(message=String) -> String")
-def echo(request, message):
-    return message
+# Register JSON-RPC methods
+import jsonrpc.tests.methods
 
 
-class SimpleTests(TestCase):
-    def setUp(self):
-        self.proxy = TestServiceProxy("/json/")
+class JSONRPCFunctionalTests(unittest.TestCase):
+    def test_method_parser(self):
+        working_sigs = [
+            ('jsonrpc', 'jsonrpc', SortedDict(), Any),
+            ('jsonrpc.methodName', 'jsonrpc.methodName', SortedDict(), Any),
+            ('jsonrpc.methodName() -> list', 'jsonrpc.methodName', SortedDict(), list),
+            ('jsonrpc.methodName(str, str, str ) ', 'jsonrpc.methodName', SortedDict([('a', str), ('b', str), ('c', str)]), Any),
+            ('jsonrpc.methodName(str, b=str, c=str)', 'jsonrpc.methodName', SortedDict([('a', str), ('b', str), ('c', str)]), Any),
+            ('jsonrpc.methodName(str, b=str) -> dict', 'jsonrpc.methodName', SortedDict([('a', str), ('b', str)]), dict),
+            ('jsonrpc.methodName(str, str, c=Any) -> Any', 'jsonrpc.methodName', SortedDict([('a', str), ('b', str), ('c', Any)]), Any),
+            ('jsonrpc(Any ) ->  Any', 'jsonrpc', SortedDict([('a', Any)]), Any),
+        ]
+        error_sigs = [
+            ('jsonrpc(str) -> nowai', ValueError),
+            ('jsonrpc(nowai) -> Any', ValueError),
+            ('jsonrpc(nowai=str, str)', ValueError),
+            ('jsonrpc.methodName(nowai*str) -> Any', ValueError)
+        ]
+        for sig in working_sigs:
+            ret = _parse_sig(sig[0], list(iter(sig[2])))
+            self.assertEquals(ret[0], sig[1])
+            self.assertEquals(ret[1], sig[2])
+            self.assertEquals(ret[2], sig[3])
+        for sig in error_sigs:
+            e = None
+            try:
+                _parse_sig(sig[0], ['a'])
+            except Exception, exc:
+                e = exc
+            self.assert_(type(e) is sig[1])
 
-    def test_simple(self):
-        response = self.proxy.echo(u"test")
-        self.assertEqual(response["error"], None)
-        self.assertEqual(response["result"], u"test")
+    def test_validate_args(self):
+        sig = 'jsonrpc(String, String) -> String'
+        M = jsonrpc_method(sig, validate=True)(lambda r, s1, s2: s1+s2)
+        self.assert_(validate_params(M, {'params': ['omg', u'wtf']}) is None)
+
+        E = None
+        try:
+            validate_params(M, {'params': [['omg'], ['wtf']]})
+        except Exception, e:
+            E = e
+        self.assert_(type(E) is InvalidParamsError)
+
+    def test_validate_args_any(self):
+        sig = 'jsonrpc(s1=Any, s2=Any)'
+        M = jsonrpc_method(sig, validate=True)(lambda r, s1, s2: s1+s2)
+        self.assert_(validate_params(M, {'params': ['omg', 'wtf']}) is None)
+        self.assert_(validate_params(M, {'params': [['omg'], ['wtf']]}) is None)
+        self.assert_(validate_params(M, {'params': {'s1': 'omg', 's2': 'wtf'}}) is None)
+
+    def test_types(self):
+        assert type(u'') == String
+        assert type('') == String
+        assert not type('') == Object
+        assert not type([]) == Object
+        assert type([]) == Array
+        assert type('') == Any
+        assert Any.kind('') == String
+        assert Any.decode('str') == String
+        assert Any.kind({}) == Object
+        assert Any.kind(None) == Nil
---
+++ jsonrpc/tests/methods.py
@@ -0,0 +1,66 @@
+from jsonrpc import jsonrpc_method
+
+
+@jsonrpc_method("jsonrpc.test")
+def echo(request, string):
+    """Returns whatever you give it."""
+    return string
+
+
+@jsonrpc_method("jsonrpc.testAuth", authenticated=True)
+def echo_auth(requet, string):
+    return string
+
+
+@jsonrpc_method("jsonrpc.notify")
+def notify(request, string):
+    pass
+
+
+@jsonrpc_method("jsonrpc.fails")
+def fails(request, string):
+    raise IndexError
+
+
+@jsonrpc_method("jsonrpc.strangeEcho")
+def strange_echo(request, string, omg, wtf, nowai, yeswai="Default"):
+    return [string, omg, wtf, nowai, yeswai]
+
+
+@jsonrpc_method("jsonrpc.safeEcho", safe=True)
+def safe_echo(request, string):
+    return string
+
+
+@jsonrpc_method("jsonrpc.strangeSafeEcho", safe=True)
+def strange_safe_echo(request, *args, **kwargs):
+    return strangeEcho(request, *args, **kwargs)
+
+
+@jsonrpc_method("jsonrpc.checkedEcho(string=str, string2=str) -> str",
+                                                      safe=True, validate=True)
+def protected_echo(request, string, string2):
+    return string + string2
+
+
+@jsonrpc_method("jsonrpc.checkedArgsEcho(string=str, string2=str)",
+                                                                 validate=True)
+def protected_args_echo(request, string, string2):
+    return string + string2
+
+
+@jsonrpc_method("jsonrpc.checkedReturnEcho() -> String", validate=True)
+def protected_return_echo(request, string, string2):
+    return string + string2
+
+
+@jsonrpc_method("jsonrpc.authCheckedEcho(Object, Array) -> Object",
+                                                                 validate=True)
+def auth_checked_echo(request, obj1, arr1):
+    return {"obj1": obj1, "arr1": arr1}
+
+
+@jsonrpc_method("jsonrpc.varArgs(String, String, str3=String) -> Array",
+                                                                 validate=True)
+def checked_var_args_echo(request, *args, **kw):
+    return list(args) + kw.values()
